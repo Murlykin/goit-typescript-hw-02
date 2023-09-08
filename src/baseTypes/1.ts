277e175a0277ccb526cca13de51bfdc22651fdc2
Andrii Murlykin <109280143+Murlykin@users.noreply.github.com>
2023-09-08 11:06:12 +0300

    1.st

--- src/baseTypes/1.ts
+++ src/baseTypes/1.ts
@@ -1,10 +1,20 @@
 // Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
+let age: number;
+let name: string;
+let toggle: boolean;
+let empty: null;
+let notInitialize: undefined;
 
-let age = 50;
-let name = 'Max';
-let toggle = true;
-let empty = null;
-let notInitialize;
-let callback = (a) => { return 100 + a };
+age = 50;
+name = 'Max';
+toggle = true;
+empty = null;
+notInitialize;
+
+let callback: (a: number) => number;
+
+callback = (a) => {
+  return 100 + a
+};
 
 export {};
